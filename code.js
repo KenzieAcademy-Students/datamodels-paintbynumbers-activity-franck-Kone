d0f95be5137081c591ea48bb50a888781580d796
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-05-08 18:31:11 +0000

    deploy: 141dddef7f05432aa0852c6a5c950967ed54081d

--- code.js
+++ code.js
@@ -92,3 +92,76 @@ let pic4 = [
    displayPixelArt(pic3)
    displayPixelArt(pic4)
 */
+displayPixelArt(pic1)
+displayPixelArt(pic2)
+displayPixelArt(pic3)
+displayPixelArt(pic4)
+function displayPixelArt(picture) {
+    
+ let section = document.createElement('section')
+ if( picture === pic1){
+    section.setAttribute('style', 'width: 240px; height: 240px; padding: 40px')
+ } else if (picture === pic2){
+     section.setAttribute('style', 'width: 240px; height: 230px; padding: 40px ')
+ } else if (picture === pic3){
+     section.setAttribute('style', 'width: 240px; height: 300px; padding: 40px ')
+ } else if (picture === pic4){
+     section.setAttribute('style', 'width: 240px; height: 210px; padding: 40px ')
+ }
+   
+    for (let row = 0; row < picture.length; row++) {
+        let rowOfPix = picture[row]
+        
+        for (let pixelColor = 0; pixelColor < rowOfPix.length; pixelColor++) {
+
+            let spanEl = document.createElement('span')
+            let pixelColors = rowOfPix[pixelColor]
+
+            switch (pixelColors) {
+                case 0:
+                    spanEl.style.backgroundColor = 'transparent';
+                    break;
+                case 1: 
+                    spanEl.style.backgroundColor = '#ffffff';
+                    break;
+                case 2: 
+                    spanEl.style.backgroundColor = '#ffc6b5';
+                    break;
+                case 3: 
+                    spanEl.style.backgroundColor = '#ff945a';
+                    break;
+                case 4: 
+                    spanEl.style.backgroundColor = '#ff3118';
+                    break;
+                case 5: 
+                    spanEl.style.backgroundColor = '#d64a00';
+                    break;
+                case 6: 
+                    spanEl.style.backgroundColor = '#c66300';
+                    break;
+                case 7: 
+                    spanEl.style.backgroundColor = '#188410';
+                    break;
+                default: 
+                    spanEl.style.backgroundColor = '#000000';
+                    
+            }
+            section.append(spanEl);
+
+        }
+    }
+  
+  let imageContainer = document.querySelector('.container')
+  imageContainer.append(section)
+}
+
+
+// 0(transparent)
+// 1	   #ffffff
+// 2	   #ffc6b5
+// 3	   #ff945a
+// 4	   #ff3118
+// 5	   #d64a00
+// 6	   #c66300
+// 7	   #188410
+// 8	   #000000
